2340bc22ebd3bc2c1ff96239bb45f2cb158b44b9
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2021-06-15 22:31:34 +0300

    qml backend

---
+++ include/QBackend.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include <qobject.h>
+
+#include "Forwards.h"
+#include "IPipelineBlock.h"
+
+class QBackend : public QObject
+{
+	Q_OBJECT
+	Q_DISABLE_COPY(QBackend)
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+	Q_PROPERTY(QString m_status READ get_status WRITE set_status NOTIFY m_status_changed)
+	Q_PROPERTY(QString m_directory_searching READ get_directory_searching WRITE set_m_directory_searching NOTIFY m_directory_searching_changed)
+	Q_PROPERTY(int m_count_handlers READ get_count_handlers WRITE set_m_count_handlers NOTIFY m_count_handlers_changed)
+	Q_PROPERTY(int m_current_page READ get_m_current_page WRITE set_m_current_page NOTIFY m_current_page_changed)
+	Q_PROPERTY(QList<QVariant> m_file_model READ get_file_model WRITE set_file_model NOTIFY m_file_model_changed)
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public:
+	QBackend();
+	~QBackend() override = default;
+
+		
+
+	QString get_directory_searching() const;
+	void set_m_directory_searching(const QString& payload);
+	QString get_status() const;
+	void set_status(const QString& payload);
+	int get_count_handlers() const;
+	void set_m_count_handlers(const int& count);
+	int get_m_current_page() const;
+	void set_m_current_page(const int& count);
+	QList<QVariant> get_file_model() const;
+	void set_file_model(const QList<QVariant>& payload);
+	
+
+	Q_INVOKABLE void open_dialog(QString path);
+	Q_INVOKABLE void start_search();
+signals:
+	void m_directory_searching_changed(QString);
+	void m_status_changed(QString);
+	void m_count_handlers_changed(int);
+	void m_current_page_changed(int);
+	void m_file_model_changed(QList<QVariant>);
+
+
+private:
+	
+
+	String m_directory_searching;
+	String m_status;
+	int m_count_handlers = 1, m_current_page = 0;
+	QList<QVariant> m_file_model;
+	
+	List<SharedPtr<IPipelineBlock>> m_pipeline;
+private slots:
+	void OnStatus(const QString& message);
+	void OnError(const QString& message, const bool& fatal = false);
+
+
+private:
+	void clean_pipeline();
+	void create_pipeline();
+
+};
---
+++ src/QBackend.cpp
@@ -0,0 +1,183 @@
+﻿#include "QBackend.h"
+
+#include <QRunnable>
+#include <QThreadPool>
+#include <QtConcurrentRun>
+
+#include "FaceDetectorBlock.h"
+#include "FileSourceBlock.h"
+#include "ResultBlock.h"
+
+
+QBackend::QBackend()
+{
+	set_m_count_handlers(1);
+	set_status("[STATUS]: Wait start...");
+}
+
+String QBackend::get_directory_searching() const
+{
+	return m_directory_searching;
+}
+
+int QBackend::get_count_handlers() const
+{
+	return m_count_handlers;
+}
+
+void QBackend::set_m_directory_searching(const QString& payload)
+{
+	m_directory_searching = std::move(payload);
+	emit m_directory_searching_changed(m_directory_searching);
+}
+
+auto QBackend::get_status() const -> String
+{
+	return m_status;
+}
+
+auto QBackend::set_status(const QString& payload) -> void
+{
+	m_status = std::move(payload);
+	emit m_status_changed(m_status);
+}
+
+
+void QBackend::set_m_count_handlers(const int& count)
+{
+	m_count_handlers = count;
+	emit m_count_handlers_changed(m_count_handlers);
+}
+
+int QBackend::get_m_current_page() const
+{
+	return m_current_page;
+}
+
+void QBackend::set_m_current_page(const int& count)
+{
+	if (m_current_page == 0) clean_pipeline();
+	
+	m_current_page = count;
+	emit m_current_page_changed(m_current_page);
+}
+
+QList<QVariant> QBackend::get_file_model() const
+{
+	return m_file_model;
+}
+
+void QBackend::set_file_model(const QList<QVariant>& payload)
+{
+	m_file_model = std::move(payload);
+	emit m_file_model_changed(m_file_model);
+}
+
+void QBackend::open_dialog(QString path)
+{
+	set_m_directory_searching(path);
+}
+
+void QBackend::start_search()
+{
+	QtConcurrent::run([&]() 
+	{
+		create_pipeline();
+	});
+	
+}
+
+void QBackend::OnStatus(const QString& message)
+{
+	set_status(String("%1%2").arg("[STATUS]: ").arg(message));
+}
+
+void QBackend::OnError(const QString& message, const bool& fatal)
+{
+	String prefix = fatal ? "[FATAL]: " : "[ERROR]: ";	
+	set_status(String("%1%2").arg(prefix).arg(message));
+}
+
+void QBackend::clean_pipeline()
+{
+	if (m_pipeline.size() <= 0) return;
+
+
+	for (int i = 0; i < m_pipeline.count(); i++)
+		if(m_pipeline[i])
+			m_pipeline[i].reset();
+
+	m_pipeline.clear();
+
+	set_status("[STATUS]: Cleanup pipeline.");
+}
+
+void QBackend::create_pipeline()
+{
+	clean_pipeline();
+
+	set_status("[STATUS]: Starting pipeline.");
+	String search_dir = get_directory_searching();
+	int count_handlers = get_count_handlers();
+
+
+	auto file_source = FileSourceBlock::create_block(search_dir);
+	auto detector = FaceDetectorBlock::create_block(count_handlers);
+	auto result_block = ResultBlock::create_block();
+
+	/////////////////////////////////////////////////////
+	m_pipeline.push_back(file_source);
+	m_pipeline.push_back(detector);
+	m_pipeline.push_back(result_block);
+	/////////////////////////////////////////////////////
+	file_source->LinkToBlock(detector);
+	detector->LinkToBlock(result_block);
+
+
+	connect(file_source.get(), &FileSourceBlock::OnStarted, this, &QBackend::OnStatus, Qt::QueuedConnection);
+	connect(file_source.get(), &FileSourceBlock::OnStoped, this, &QBackend::OnStatus, Qt::QueuedConnection);
+
+	connect(detector.get(), &FaceDetectorBlock::OnStarted, this, &QBackend::OnStatus, Qt::QueuedConnection);
+	connect(detector.get(), &FaceDetectorBlock::OnStoped, this, &QBackend::OnStatus, Qt::QueuedConnection);
+
+	connect(result_block.get(), &ResultBlock::OnStarted, this, &QBackend::OnStatus, Qt::QueuedConnection);
+	connect(result_block.get(), &ResultBlock::OnStoped, this, &QBackend::OnStatus, Qt::QueuedConnection);
+
+	connect(file_source.get(), &FileSourceBlock::OnError, this, &QBackend::OnError, Qt::QueuedConnection);
+	connect(detector.get(), &FaceDetectorBlock::OnError, this, &QBackend::OnError, Qt::QueuedConnection);
+	connect(result_block.get(), &ResultBlock::OnError, this, &QBackend::OnError, Qt::QueuedConnection);
+
+
+
+
+	connect(file_source.get(), &FileSourceBlock::OnStarted, this, [&](const String& message)
+		{
+			set_m_current_page(1);
+		}, Qt::QueuedConnection);
+	connect(detector.get(), &FaceDetectorBlock::OnStarted, this, [&](const String& message)
+		{
+			set_m_current_page(1);
+		}, Qt::QueuedConnection);
+	connect(result_block.get(), &ResultBlock::OnStoped, this, [&, result_block](const String& message)
+		{
+			set_m_current_page(2);
+			List<String> images;
+			if (result_block)
+				images = result_block->GetResult();
+
+			set_status(String("[STATUS]: Found image with faces - %1").arg(images.count()));
+
+			QList<QVariant> for_model;
+
+			for (auto& path : images)
+				for_model.push_back(QVariant::fromValue(std::move(path)));
+
+			set_file_model(for_model);
+
+			clean_pipeline();
+		}, Qt::QueuedConnection);
+
+
+	file_source->Start();	
+	
+}
