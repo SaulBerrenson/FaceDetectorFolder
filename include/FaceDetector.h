6efb31c81044533d4e8d611228fbe6f0cc890a71
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2021-06-15 22:30:13 +0300

    module face detector

---
+++ include/FaceDetector.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <opencv2/opencv.hpp>
+
+#include "Forwards.h"
+
+class FaceDetector
+{
+public:
+	static SharedPtr<FaceDetector> create();
+	
+	bool set_image(const String& path_to_image);
+	bool try_found_any_face();
+
+public:
+	FaceDetector();
+	~FaceDetector() = default;
+private:	
+	cv::CascadeClassifier	m_face_detector;
+	cv::Mat					m_image;
+	const cv::String		m_model_name = "model_face.xml";
+};
---
+++ src/FaceDetector.cpp
@@ -0,0 +1,42 @@
+﻿#include "FaceDetector.h"
+
+#include <qdir.h>
+#include <QFile>
+
+SharedPtr<FaceDetector> FaceDetector::create()
+{
+	return SharedPtr<FaceDetector>(new FaceDetector());
+}
+
+FaceDetector::FaceDetector()
+{
+	auto path = QDir::cleanPath(QDir::currentPath() + QDir::separator() + QString::fromStdString(m_model_name));
+	if (!QFile(path).exists()) { throw std::exception("File is not exist!"); return; }	
+	m_face_detector.load(path.toStdString());
+}
+
+bool FaceDetector::set_image(const String& path_to_image)
+{
+	if (!QFile(path_to_image).exists()) return false;
+
+	m_image.release();
+	m_image = imread(path_to_image.toStdString(), cv::IMREAD_COLOR);
+
+}
+
+bool FaceDetector::try_found_any_face()
+{
+	if (m_image.empty()) return false;
+	std::vector<cv::Rect> faces;
+
+	cv::Mat img_gray;
+	cv::cvtColor(m_image, img_gray, cv::COLOR_BGR2GRAY);
+	m_image.release();
+	m_face_detector.detectMultiScale(img_gray, faces, 1.1, 2, 0 | 2, cv::Size(10, 10));
+	img_gray.release();
+	
+	if(faces.size() > 0) return true;
+	return false;	
+}
+
+
