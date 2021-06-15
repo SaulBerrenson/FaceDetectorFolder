05810782d8072fca1368592121e2e1dc11cc078f
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2021-06-15 22:31:21 +0300

    result block at pipeline

---
+++ include/ResultBlock.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "IPipelineBlock.h"
+
+class ResultBlock : public IPipelineBlock
+{
+public:
+	static SharedPtr<ResultBlock> create_block();
+
+public slots:
+	bool Start() override;
+	bool Stop() override;
+public:
+	ResultBlock() = default;
+	~ResultBlock() override = default;
+
+	List<String> GetResult();
+
+private:
+	List<String> m_files_with_faces;
+};
---
+++ src/ResultBlock.cpp
@@ -0,0 +1,38 @@
+﻿#include "ResultBlock.h"
+
+#include <qmutex.h>
+
+SharedPtr<ResultBlock> ResultBlock::create_block()
+{
+	return SharedPtr<ResultBlock>(new ResultBlock);
+}
+
+bool ResultBlock::Start()
+{
+	emit OnStarted("Result Block is started!");
+	auto payload = this->property(this->m_payload_name);
+
+	if (!payload.isValid() || payload.isNull()) return false; 
+
+	for (const auto& path : payload.value<List<String>>())
+	{
+		m_files_with_faces.push_back(std::move(path));
+	}
+	//replace value for block
+	this->setProperty(this->m_payload_name, "");
+	
+	call_next_block();
+
+	return true;
+}
+
+bool ResultBlock::Stop()
+{
+	emit OnStoped("Result Block was stopped!");
+	return true;
+}
+
+List<String> ResultBlock::GetResult()
+{
+	return std::move(this->m_files_with_faces);
+}
