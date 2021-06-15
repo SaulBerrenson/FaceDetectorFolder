2dfaf8f10e2b29c278ba73fbd48d65c9a500fd29
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2021-06-15 22:31:47 +0300

    Create Forwards.h
    forward declaration

---
+++ include/Forwards.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <qdir.h>
+#include <qstring.h>
+#include <qsharedpointer.h>
+#include <qscopedpointer.h>
+#include <QWeakPointer>
+#include <QQueue>
+
+
+using String = QString;
+
+#define  Separator = QDir::separator();
+
+
+template<class T>
+using List = QVector<T>;
+
+template <class Key,class Value>
+using Dictionary = QMap<Key, Value>;
+
+template<class T>
+using Queue = QQueue<T>;
+//////////////////////////////////////////
+template<class T>
+using SharedPtr = QSharedPointer<T>;
+template<class T>
+using UniquePtr = QScopedPointer<T>;
+template<class T>
+using WeakPtr = QWeakPointer<T>;
