43aa6f14e58f52b89d61965e8bd400556d6b61c7
SaulBerrenson <64542974+SaulBerrenson@users.noreply.github.com>
2021-06-15 22:29:53 +0300

    Create FileSourceBlock.h

---
+++ include/FileSourceBlock.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "IPipelineBlock.h"
+
+
+class FileSourceBlock final : public IPipelineBlock 
+{
+public:
+	explicit FileSourceBlock(const String& m_dir)
+		: m_dir(m_dir)
+	{
+
+	}
+
+	~FileSourceBlock() override;
+
+	static SharedPtr<FileSourceBlock> create_block(const String& dir);
+	
+public slots:
+	bool Start() override;
+	bool Stop() override;
+
+private:
+	String m_dir;
+	List<String> m_files;
+	const String m_extantion = "*.jpg";
+
+
+};
